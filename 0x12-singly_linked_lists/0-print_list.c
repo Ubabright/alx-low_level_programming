16b5ef269ae349269745d9af9dee56511fb3a6d8
Ubabright <hndrxxishigh@gmail.com>
2023-07-28 11:41:58 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 {
 	size_t total = 0;
 
-	while (h != NULL)
+	for (;h != NULL; total++)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		total++;
+		
 	}
 
 	return (total);
