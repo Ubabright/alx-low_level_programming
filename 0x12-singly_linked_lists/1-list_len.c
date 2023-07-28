434e81a8b2203f15fd28380112a321c5bb33da7c
Ubabright <hndrxxishigh@gmail.com>
2023-07-28 09:10:01 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * list_len - Views the numbers of elements in a list
+  * @h: A linked list
+  *
+  * Return: No. of elements in a list
+  */
+size_t list_len(const list_t *h)
+{
+	size_t total = 0;
+
+	while (h = NULL)
+	{
+		h = h->next;
+		total++;
+	}
+
+	return (total);
+}
