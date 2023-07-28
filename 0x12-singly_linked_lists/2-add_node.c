b751e482386ad61ae30d6fc683782e4d47a9a902
Ubabright <hndrxxishigh@gmail.com>
2023-07-28 14:53:53 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,15 +13,15 @@ list_t *add_node(list_t **head, const char *str)
 {
  list_t *new;
  unsigned int lent = 0;
- while (str[lent])
- lent++;
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
- new->str = strdup(str);
- new->lent = lent;
- new->next = (*head);
- (*head) = new;
- return (*head);
+	 while (str[lent])
+         lent++;
+	 new = malloc(sizeof(list_t));
+	 if (!new)
+		 return (NULL);
+                 new->str = strdup(str);
+                 new->lent = lent;
+                 new->next = (*head);
+                 (*head) = new;
+	 return (*head);
 }
 
