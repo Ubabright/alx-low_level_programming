12a8e818e4c2cce79ab5cf28aa7420150899ddf5
Ubabright <hndrxxishigh@gmail.com>
2023-07-28 21:37:27 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,13 +8,12 @@
 void free_list(list_t *head)
 {
 	list_t *ptr;
-
- while (head)
- {
-	 ptr = head->next;
-         free(head->str);
-         free(head);
-         head = ptr;
- }
+	
+	while (head)
+	{
+		ptr = head->next;
+		free(head->str);
+                free(head);
+		head = ptr;
+	}
 }
-
