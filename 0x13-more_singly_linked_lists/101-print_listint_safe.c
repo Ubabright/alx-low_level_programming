6c503e511ecef25fa03065ce2b95b5ac93de47e2
Ubabright <hndrxxishigh@gmail.com>
2023-08-02 06:47:38 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,54 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t print_listint_safe(const listint_t *head);
-
-/**
- * looped_listint_len - calculates the amount of important nodes in a looped listint_t linked list.
- * @head: points to the head of the listint_t 
- *
- * Return: return 0 if the list is not looped, else the number of important nodes in the list
- */
-size_t looped_listint_len(const listint_t *head)
-{
-	const listint_t *boc, *haa;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	boc = head->next;
-	haa = (head->next)->next;
-
-	while (haa)
-	{
-		if (boc == haa)
-		{
-			boc = head;
-			while (boc != haa)
-			{
-				nodes++;
-				boc = boc->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (boc != haa)
-			{
-				nodes++;
-				boc = boc->next;
-			}
-
-			return (nodes);
-		}
-
-		boc = boc->next;
-		haa = (haa->next)->next;
-	}
-
-	return (0);
-}
-
 /**
  * print_listint_safe - Prints a listint_t list 
  * @head:  points to the head of the listint_t list.
@@ -57,13 +9,13 @@ size_t looped_listint_len(const listint_t *head)
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index = 0;
+	size_t boc, ind = 0;
 
-	nodes = looped_listint_len(head);
+	boc = looped_listint_len(head);
 
-	if (nodes == 0)
+	if (boc == 0)
 	{
-		for (; head != NULL; nodes++)
+		for (; head != NULL; boc++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
@@ -72,7 +24,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	else
 	{
-		for (index = 0; index < nodes; index++)
+		for (ind = 0; ind < boc; ind++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
@@ -81,5 +33,5 @@ size_t print_listint_safe(const listint_t *head)
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
-	return (nodes);
+	return (boc);
 }
