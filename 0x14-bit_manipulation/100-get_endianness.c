8e16b26a992a924355b7954e9f362451c01ebd67
Ubabright <hndrxxishigh@gmail.com>
2023-08-03 14:13:56 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ *
+ * Return: return 0 if big endian, else  1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int y;
+	char *a;
+
+	y = 1;
+	a = (char *) &y;
+
+	return ((int)*a);
+}
